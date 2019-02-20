b554b13ce34734e3f6fa361f87b082c7505c9728
luiscript <isc.luisfer@gmail.com>
2019-02-20 00:35:14 -0600

    Progress on Machine learning. Starting point for openCV.js

--- Machine-learning-with-JS/knn-tf/index.js
+++ Machine-learning-with-JS/knn-tf/index.js
@@ -2,12 +2,47 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
+function knn(features, labels, predictionPoint, k){
+  const { mean, variance } = tf.moments(features, 0);
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
+  return features
+    .sub(mean)
+    .div(variance.pow(0.5))
+    .sub(scaledPrediction)
+    .pow(2)
+    .sum(1)
+    .pow(0.5)
+    .expandDims(1)
+    .concat(labels, 1)
+    .unstack()
+    .sort((a,b)=> a.get(0)> b.get(0) ? 1 : -1)
+    .slice(0,k)
+    .reduce((acc, pair) => acc + pair.get(1), 0) / k;
+}
+
 let {features, labels, testFeatures, testLabels } =  loadCSV('kc_house_data.csv', {
   shuffle: true,
   splitTest: 10,
-  dataColumns: ['lat', 'long'],
+  dataColumns: ['lat', 'long', 'sqft_lot'],
   labelColumns: ['price']
 });
 
-console.log(testFeatures);
-console.log(testLabels);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+//testFeatures = tf.tensor(testFeatures);
+//testLabels = tf.tensor(testLabels);
+
+
+testFeatures.forEach((testPoint, i)=>{
+  const result = knn(features, labels, tf.tensor(testPoint), 10);
+  const err = (testLabels[i][0] - result) / testLabels[i][0];
+  console.log("error", err * 100);
+})
+
+
+// console.log("Guess", result, testLabels[0][0]);
+
+
+// console.log(testFeatures);
+// console.log(testLabels);

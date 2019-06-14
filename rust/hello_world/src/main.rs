00da6fdd97989982b72fd9728aa80240bae7faad
luiscript <isc.luisfer@gmail.com>
2019-06-13 19:51:34 -0500

    Rust operators

--- rust/hello_world/src/main.rs
+++ rust/hello_world/src/main.rs
@@ -1,7 +1,50 @@
 use std::mem;
 
-fn main() {
-    println!("Hello, world!");
+fn operators()
+{
+    //arithmetic
+    let mut a =  2+3*4;
+    println!("a = {}", a); 
+    a = a+1; // -- o ++ no existen en rust
+    println!("a = {}", a);
+    a += 1;  // pero si exiten -= += *= /= %=
+    println!("a = {}", a);
+
+    println!("reminder of {} / {} = {}", a, 3, a%3);
+
+    let a_cubed = i32::pow(a, 3);
+    println!("{} cubed is {}", a, a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+
+    println!("{} cubed = {}, {} ^ PI = {}", b, b_cubed, b, b_to_pi);
+
+
+    // bitwise
+
+    let c = 1 | 2;  // | OR  & AND  ^ XOR    ! NOR
+    println!("1|2 = {}", c);
+
+    let one_to_10 = 1 << 10;
+    println!("2^10 = {}", one_to_10);
+
+
+    //logical
+
+    let pi_less_4 = std::f64::consts::PI < 4.0;
+    println!("PI < 4 {}", pi_less_4);
+
+    let x = 5;
+    let x_is_5 = x == 5;
+
+    println!("x == 5 {}", x_is_5);
+
+}
+
+fn basic_types()
+{
 
     // inmutable unsigned 8 bits integer 0...255 
     let a:u8 = 123;  
@@ -34,4 +77,10 @@ fn main() {
 
     let g = false;
     println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+}
+
+fn main() {
+    println!("Hello, world!");
+    basic_types();
+    operators();
 }
